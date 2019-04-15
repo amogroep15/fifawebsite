fba55e66c197fd00e20ee3e6e1ee5d95495d511a
Unknown <h.fm@live.com>
2019-04-15 11:09:33 +0200

    d

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hfm
+ * Date: 15-4-2019
+ * Time: 11:07
+ */
+?>
+<footer>
+    &copy; copyright Kas Rasenberg, Daniel vahabi, Rick van Zelst en Hassan Hassan, AMO Groep 15 <?php echo date('d-m-y');?>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hfm
+ * Date: 15-4-2019
+ * Time: 11:07
+ */
+
+require 'config.php';
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>FifaBet</title>
+</head>
+<body>
