0c63bab657eb425f577c441dc0f7bf853681cba2
Rick <rickvanzelst@live.nl>
2019-04-18 09:15:10 +0200

    Home Page afgemaakt en Login button

--- master.php
+++ master.php
@@ -1,7 +1,18 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: hfm
- * Date: 15-4-2019
- * Time: 11:07
- */
+<?php require 'header.php'; ?>
+
+
+<div class="homepage">
+<div class = "login">
+    <div class="download"
+<h1>Download hier de applicatie: </h1>
+    <HR>
+    <a href="#" class="loginButton">Download</a>
+</div>
+<div class = "login1"
+<h1>Log in om een schema te maken.</h1>
+<HR>
+    <a href="#" class="loginButton">Log in</a>
+</div>
+</div>
+
+<?php require 'footer.php'; ?>
