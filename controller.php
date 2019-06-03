afdb75bb154de954dd34da38a073234eb47dee39
Kas <kasrasenberg@gmail.com>
2019-06-03 09:46:12 +0200

    zelfmoordneigingen

--- controller.php
+++ controller.php
@@ -8,13 +8,31 @@
 require 'config.php';
 
 
-/*COMPITION FUNCTION*/
 
 if ($_POST['type'] == 'create-competition') {
 
-    $sqlreset = "TRUNCATE TABLE matches";
-    $querydel = $db->query($sqlreset); //verzoek naar de database, voer sql van hierboven uit
+    $querydel = $db->query("TRUNCATE TABLE matches"); // reset database matches
+    $querydel = $db->query("TRUNCATE TABLE match_requirements");
+    
 
+    $start_time =  $_POST['start_time'];
+    $match_length = trim($_POST['match_length']);
+    $rest = trim($_POST['rest']);
+    $break = trim($_POST['break']);
+
+    $sql = "INSERT INTO match_requirements (start_time, match_length, rest, break) VALUES (:start_time, :match_length, :rest, :break)";
+    $prepare = $db->prepare($sql);
+    $prepare->execute([
+        'start_time'     => $start_time,
+        'match_length'   => $match_length,
+        'rest'           => $rest,
+        'break'          => $break
+    ]);
+
+
+
+
+    //
 
     $teamsql = "SELECT * FROM teams";
     $query = $db->query($teamsql); //verzoek naar de database, voer sql van hierboven uit
@@ -35,17 +53,12 @@ if ($_POST['type'] == 'create-competition') {
         $yourdatetime = $match_requirement['start_time'];       /*Pakt de tijd uit de data base in een 'time' format*/
         $minutes = $match_requirement['match_length'] + $match_requirement['rest'] + $match_requirement['break'];   /*Hier bereken ik de aantal minuten
         tot de andere wedstrijd (aantal minuten van de wedstrijd tussenpauze en pauze*/
-
         $length_match = $match_requirement['match_length'];
         $length_rest  = $match_requirement['rest'];
         $length_break = $match_requirement['break'];
-//
-//        $matchtime = date('h:i' ,$length_match);
-//        $rest = date('h:i', $length_rest);
-//        $break = date('h:i', $length_break);
+//      
 
-        $timestamp = strtotime($yourdatetime) + $minutes * 60;
-
+        $timestamp = $yourdatetime + $minutes * 60;
 
     }
     $fieldcount = count($fields);
@@ -80,32 +93,33 @@ if ($_POST['type'] == 'create-competition') {
                     $r ++;
                 }
                 $currentfield = $allfields[$r];
+                
 
-
-
-
-                $matchsql = "INSERT INTO matches ( team1, team2, length_match, length_rest, length_break ,field_id)
-                        values(:team1, :team2, :length_match, :length_rest, :length_break ,:field_id)";
-                $prepare = $db->prepare($matchsql);
+                $sql = "INSERT INTO matches (team1, team2, start_timestamp, length_match, length_rest, length_break ,field_id)
+                        values(:team1, :team2, :start_timestamp, :length_match, :length_rest, :length_break ,:field_id)";
+                $prepare = $db->prepare($sql);
                 $prepare->execute([
                     ':team1' => $teamsArray[$i],
                     ':team2' => $teamsArray[$x],
+                    ':start_timestamp' => date('H:i:s', $start_time),
                     ':length_match' => $length_match,
                     ':length_rest' => $length_rest,
                     ':length_break' => $length_break,
                     ':field_id' => $currentfield
                 ]);
+
+                $start_time = strtotime("+$minutes minutes", strtotime($yourdatetime));
+                
             }
         }
     }
 
-    //exit;
     header("Location: matches.php");
     exit;
 }
 
 /*DELETE FIELD FUNCTION*/
-if ( $_POST['type'] === 'delete_field'){
+if ($_POST['type'] === 'delete_field'){
     $id = $_GET['id'];
 
     $sql = " DELETE from fields WHERE id = :id";
@@ -141,6 +155,7 @@ if ($_POST['type'] === 'addfield'){
     $fieldname = htmlentities(trim($_POST['fieldname']));
 
 
+    
     $sql = "INSERT INTO fields (fieldname) VALUES (:fieldname)";
 
     $prepare = $db->prepare($sql);
@@ -149,9 +164,8 @@ if ($_POST['type'] === 'addfield'){
 
     ]);
 
-    $msg = "Veld is succesvol toegevoegd!";
 
-    header("location: fields.php?message=$msg");
+    header("location: fields.php?success=fields");
     exit;
 }
 if ($_POST['type'] === 'scores') {
--- generatematch.php
+++ generatematch.php
@@ -1,18 +1,29 @@
 <?php require 'header.php';
 
+$sql = "SELECT * FROM matches";
+    $prepare = $db->prepare($sql);
+    $prepare->execute([]);
+    $result = $prepare->fetch();
+    if($result != false){
+        header('Location: matches.php');
+        exit();
+    }
+
+
 ?>
 
 <form action="controller.php" method="post">
     <input type='hidden' name='type' value='create-competition'>
-<!---->
-<!--    <label for="match_timestamp">Begin tijd van de wedstrijden</label>-->
-<!--    <input type="time" name="match_timestamp" id="match_timestamp">-->
-<!---->
-<!--    <input type="number" name="field_id" id="field_id">-->
-
+    <label for="start_time">Begin tijd van de wedstrijden</label>
+    <input type="time" name="start_time" id="start_time">
+    <label for="match_length">Wedstrijdsduur (minuten)</label>
+    <input type="text" name="match_length" id="match_length">
+    <label for="rest">Tussenpauze minuten</label>
+    <input type="text" name="rest" id="rest">
+    <label for="break">Pauze</label>
+    <input type="text" name="break" id="break">
     <button type="submit">Wedstrijdschema Maken</button>
 </form>
 
 
-<?php require 'footer.php';
-?>
+<?php require 'footer.php';?>
